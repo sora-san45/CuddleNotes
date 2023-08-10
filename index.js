138e8e80a90157c958f7f3130f5f3d4225265df4
Mary Ann Jose <maryannjose129@gmail.com>
2023-08-10 19:12:34 +0530

    changes

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 
 function Popfunc(){
     document.querySelector(".popup").style.visibility="visible";
+    document.querySelector(".overlay").style.visibility="visible"
 }
 function Popclose(){
     document.querySelector(".popup").style.visibility="hidden";
+    document.querySelector(".overlay").style.visibility="hidden";
 }
