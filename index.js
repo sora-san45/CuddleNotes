eeccc78fbe12a0bb910a3e8a9df1c23992209309
Mary Ann Jose <maryannjose129@gmail.com>
2023-09-11 08:48:43 +0530

    commits

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-
-function Popfunc(){
-    document.querySelector(".popup").style.visibility="visible";
-    document.querySelector(".overlay").style.visibility="visible"
+const popupBox = document.getElementById("popup")
+const overlay = document.getElementById("overlay")
+const addBtn = document.getElementById("add-btn")
+const save = document.getElementById("save")
+const close = document.getElementById("close")
+const notesContainer = document.getElementById("notes-container")
+function openPopup() {
+    popupBox.style.visibility = "visible"
+    overlay.style.visibility = "visible"
 }
-function Popclose(){
-    document.querySelector(".popup").style.visibility="hidden";
-    document.querySelector(".overlay").style.visibility="hidden";
+function closePopup() {
+    popupBox.style.visibility = "hidden"
 }
 
-const listnotes =document.getElementsByClassName(".notes");
+addBtn.addEventListener("click", function () {
+    openPopup()
+})
+
+save.addEventListener("click", function () {
+    const inputVal = document.getElementById("inputField").value
+    notesContainer.innerHTML += `<div class="note">
+    <div class="note-banner">
+        <img src="images/bg-cute.png">
+    </div>
+    <div class="text">
+        ${inputVal}
+    </div>
+    <div class="ops">
+        <i class="fa fa-trash" style="color: #c09268; font-size: 1.1rem"></i>
+    </div>
+</div>`
+    closePopup()
+})
+
+close.addEventListener("click", function () {
+    closePopup()
+})
